eb0dd4dc3a0a20493cb9ccc6a68709ca7fb9240c
333am0 <87911535+333am0@users.noreply.github.com>
2023-06-08 04:53:54 -0400

    completed navbar,typing animation

---
+++ portfolio/src/components/Work.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import WorkItem from './WorkItem'
+
+const data = [
+    {
+        year: '2019',
+        title: 'Software Engineer',
+        duration: '2019 - Present',
+        details: 'Software Engineer'
+        
+    },
+    {
+        year: '2019',
+        title: 'Software Engineer',
+        duration: '2019 - Present',
+        details: 'Software Engineer'
+        
+    },
+    {
+        year: '2019',
+        title: 'Software Engineer',
+        duration: '2019 - Present',
+        details: 'Software Engineer'
+        
+    }
+]
+
+const Work = () => {
+  return (
+    <div id='work' className='max-w[1040px] m-auto md:pl-20mp-4 py-16'> 
+    <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+    {data.map((item, idx) => (
+        <WorkItem
+        key={idx}
+        year={item.year}
+        title={item.title}
+        duration={item.duration}
+        details={item.details}
+        />
+    ))}
+    </div>
+
+  )
+}
+
+export default Work
---
+++ portfolio/src/components/main.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from'react-icons/fa'
+
+const Main = () => {
+  return (
+    <div id='main'>
+        <img className='w-full h-screen object-cover object-left' 
+        src='' 
+        alt=''>    
+        </img>
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+          <h2 className='flex sm:text-5xl text-4xl pt-4 text-gray-800'>
+        <TypeAnimation
+      sequence={[
+        'My Name Is Aaron', // Types 'One'
+        1000, // Waits 1s
+        'Im a Full Stack Developer', // Deletes 'One' and types 'Two'
+        2000, // Waits 2s
+        'I Love Software Development', // Types 'Three' without deleting 'Two'
+        3000, // Waits 3s
+        () => {
+          console.log('Sequence completed'); // Place optional callbacks anywhere in the array
+        }
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '1em', paddingLeft: '5px' }}
+    />
+    </h2>
+        <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+          <FaLinkedin className='cursor-pointer' size={20}/>
+          <FaTwitter className='cursor-pointer' size={20}/>
+          <FaGithub className='cursor-pointer' size={20}/>
+          <FaInstagram className='cursor-pointer' size={20}/>
+        </div>
+        </div>
+        </div>
+   </div>
+  )
+}
+
+export default Main
